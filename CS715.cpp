60a717430ed7a0fb51df72bc97605a8071bab391
Josh Hill <josh.hill189@gmail.com>
2018-08-18 12:39:39 +1200

    Add main files
    Using CMake for the moment as I know how that one works

---
+++ CS715.cpp
@@ -0,0 +1,13 @@
+﻿// CS715.cpp : Defines the entry point for the application.
+//
+
+#include "CS715.h"
+
+int main()
+{
+
+	for (int i = 0; i < 100000; i++) {  // Just to make sure it actually works
+		std::cout << "Hello CMake." << std::endl;
+	}
+	return 0;
+}
